55324c311f7908c1c204844c3af4c0f3132f6333
Pravash Ghimire <ghpravash21@gmail.com>
2021-05-05 20:41:51 +0545

    updated

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import './index.css';
 
-const Moviecard = ({movie}) => {
-    return ( 
-        <div className="card">
-            <img className="card--image" src={`https://image.tmdb.org/t/p/w195_and_h278_bestv2/${movie.poster_path}`}
-                alt={movie.title} />
-            <div className="card--content">
-                <h3 className="card--title">{movie.title}</h3>
-                <p><small>Release Date: {movie.release_date}</small></p>
-                <p><small>Rating: {movie.vote_average}</small></p>
-                <p className="card--desc">{movie.overview}</p>
+const Moviecard = ({ movie }) => {
+    return (
+        <div className="container">
+            <div className="card">
+                <img className="card--image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                    alt={movie.title + ' poster'} />
+                <div className="card--content">
+                    <h3 className="card--title">{movie.title}</h3>
+                    <p>Release Date: {movie.release_date}</p>
+                    <p>Rating: {movie.vote_average}</p>
+                    <p className="card--desc">{movie.overview}</p>
+                </div>
             </div>
-        </div> 
+        </div>
     )
 }
 
--- src/Searchmovie.js
+++ src/Searchmovie.js
@@ -1,50 +1,54 @@
 import React, { useState } from 'react';
 import './index.css';
 import Moviecard from './Moviecard';
+import "./App.css";
+// import ReactPaginate from 'react-paginate';
 
 export default function Searchmovie() {
     //Two states one for input query and another for displaying movie
     //for input query
-    const [query, setQuery] = useState('');
+    const [query, setQuery] = useState('startups');
     //for movie 
     const [movies, setMovies] = useState([]);
 
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=64861e909c80f895381498f7db521436&language=en-US&query=${query}`
 
-    const Searchmovie = async (e) => {
+
+    const searchMovie = (e) => {
+
         e.preventDefault();
 
-        console.log("submitting");
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setMovies(data.results);
+            })
+            .catch(error => console.error('Error', error));
+    };
 
-
-
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=64861e909c80f895381498f7db521436&language=en-US&query=${query}&page=1&include_adult=false`;
-
-        try {
-            const res = await fetch(url);
-            const data = res.json();
-            console.log(data.results);
-            setMovies(data.results);
-        } catch (err) {
-            console.error(err);
-        }
-    }
     return (
         <>
-            <form className="form" onSubmit={Searchmovie}>
+            <form className="form">
                 <label className="label" htmlFor="query">Movie Name:</label>
                 {/* value={query} connects input to useState */}
-                <input className="input" type="text" name="query" placeholder="i.e. Ice Age" value={query} onChange={(e) => setQuery(e.target.value)} />
-                <button className="button" type="submit">Search</button>
+                <input className="input" type="text" name="query" placeholder="i.e. ice age" onChange={(e) => setQuery(e.target.value)} />
+                <button className="button" type="submit" onClick={searchMovie}>Search</button>
             </form>
-            <div className="card-list">
-                {movies.map(movie => {
-                    return ( <Moviecard movie={movie} key={movie.id}/> ) 
-                    })}
-            </div>
-        </>
-        )
-    }
 
-    // .filter((movie) => movie.poster_path)
+            <div className="card-list">
+                {
+                    movies.filter((movie) => movie.poster_path).map((movie) => {
+                        return (
+                            <Moviecard movie={movie} key={movie.id} />
+                        );
+                    })
+                }
+            </div>
+
+        </>
+    );
+
+}
+
 
 
